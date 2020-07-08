0410caaf666c33ed3ab70e056577aa49c93c2b87
Felipe Batista <feliperesource@gmail.com>
2020-07-07 22:46:55 -0300

    Primeiro commit do projeto com tabela criada com models

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from core.models import Evento
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+# Register your models here.
+admin.site.register(Evento, EventoAdmin)
